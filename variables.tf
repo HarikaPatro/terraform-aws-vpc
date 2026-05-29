e350ce03188a15a27fc47099111a3d61ad3c499b
HarikaPatro <patroharika866@gmail.com>
2026-05-29 15:36:01 +0530

    vpc

--- variables.tf
+++ variables.tf
@@ -15,3 +15,51 @@ variable "vpc_tags" {
     type = map
     default = {}
 }
+
+variable "igw_tags" {
+    type = map
+    default = {}
+}
+
+variable "public_subnet_cidrs" {
+    type = list
+}
+
+variable "public_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_subnet_cidrs" {
+    type = list
+}
+
+variable "private_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_subnet_cidrs" {
+    type = list
+}
+
+variable "database_subnet_tags" {
+    type = map
+    default = {}
+}
+
+variable "public_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map
+    default = {}
+}
+
